ece9e463ce0d77ef98fe5225055c0bfd4d2a2dab
heeralalkumarheera <heeralalkumarheera@gmail.com>
2025-04-17 09:28:05 +0530

    17.4

--- Project/calculator-version-one/calculator/src/App.jsx
+++ Project/calculator-version-one/calculator/src/App.jsx
@@ -1,14 +1,16 @@
 import Display from "./components/Display";
 import styles from "./App.module.css";
 import ButtonsContainer from "./components/ButtonsContainer";
-function App() {
-   
+import { useState } from "react";
 
+function App() {
+  const [calValue, setCalValue] = useState("");
+  const onButtonClick = (buttonText) => console.log(buttonText)
   return (
-     <div className={styles.calculator}>
-       <Display></Display>
-       <ButtonsContainer></ButtonsContainer>
-     </div>
+    <div className={styles.calculator}>
+      <Display displayValue={calValue}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
   )
 }
 
--- Project/calculator-version-one/calculator/src/components/ButtonsContainer.jsx
+++ Project/calculator-version-one/calculator/src/components/ButtonsContainer.jsx
@@ -1,10 +1,13 @@
 import styles from './ButtonsContainer.module.css';
 
-const ButtonsContainer = () => {
+const ButtonsContainer = ({ onButtonClick }) => {
     const ButtonNames = ['C', '1', '2', '+', '3', '4', '-', '5', '6', '*', '7', '8', '/', '=', '9', '0'];
     return (
         <div className={styles.buttonsContainer}>
-            {ButtonNames.map((ButtonNames, index) => (<button key={index} className={styles.button}>{ButtonNames}</button>))}
+            {ButtonNames.map((ButtonNames, index) =>
+            (<button key={index} className={styles.button} onClick={() => onButtonClick(ButtonNames)}>
+                {ButtonNames}
+            </button>))}
 
         </div>
     );
