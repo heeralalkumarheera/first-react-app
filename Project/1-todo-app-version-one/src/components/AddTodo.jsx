5a90e8a115859f2cc664fa29eb99db2e2983cb56
heeralalkumarheera <heeralalkumarheera@gmail.com>
2025-05-06 09:36:13 +0530

    06/05/25

--- Project/1-todo-app-version-one/src/components/AddTodo.jsx
+++ Project/1-todo-app-version-one/src/components/AddTodo.jsx
@@ -1,21 +1,50 @@
-function AddTodo() {
-    return (
-      <div className="container text-center">
-        <div className="row kg-row">
-          <div className="col-6">
-            <input type="text" placeholder="Enter Todo Here" />
-          </div>
-          <div className="col-4">
-            <input type="date" />
-          </div>
-          <div className="col-2">
-            <button type="button" className="btn btn-success kg-button">
-              Add
-            </button>
-          </div>
+import { useState } from "react";
+import { IoMdAddCircle } from "react-icons/io";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState();
+  const [dueDate, setDueDate] = useState();
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    onNewItem(todoName, dueDate);
+    setDueDate("");
+    setTodoName("");
+  };
+
+  return (
+    <div className="container text-center">
+      <div className="row kg-row">
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            value={todoName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" value={dueDate} onChange={handleDateChange} />
+        </div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-success kg-button"
+            onClick={handleAddButtonClicked}
+          >
+            <IoMdAddCircle />
+          </button>
         </div>
       </div>
-    );
-  }
-  
-  export default AddTodo;
+    </div>
+  );
+}
+
+export default AddTodo;
--- Project/1-todo-app-version-one/src/components/TodoItem.jsx
+++ Project/1-todo-app-version-one/src/components/TodoItem.jsx
@@ -1,17 +1,22 @@
-function TodoItem({ todoName, todoDate }) {
-    return (
-      <div className="container">
-        <div className="row kg-row">
-          <div className="col-6">{todoName}</div>
-          <div className="col-4">{todoDate}</div>
-          <div className="col-2">
-            <button type="button" className="btn btn-danger kg-button">
-              Delete
-            </button>
-          </div>
+import { MdDeleteForever } from "react-icons/md";
+function TodoItem({ todoName, todoDate, onDeleteClick }) {
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-danger kg-button"
+            onClick={() => onDeleteClick(todoName)}
+          >
+            <MdDeleteForever />
+          </button>
         </div>
       </div>
-    );
-  }
-  
-  export default TodoItem;
+    </div>
+  );
+}
+
+export default TodoItem;
